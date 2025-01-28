2f89bf58dfbfd025e1bb1ab462613d80a34e2e1a
Giovanni Miro <giovannimiropl@hotmail.com>
2025-01-27 23:09:49 -0300

    Models, Routes and Requirements update

--- models/schemas.py
+++ models/schemas.py
@@ -1,6 +1,22 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validates, ValidationError
 #Base schema class with which to define schemas.
 
 class UserSchema(Schema):
     id = fields.Int(dump_only=True)
-    username = fields.Str()
+    username = fields.Str(required=True)
+    password = fields.Str(required=True, load_only=True)
+    balance = fields.Numeric(default=0.00)
+    updated_at = fields.DateTime(dump_only=True)
+
+    @validates("password")
+    def password_validation(self, value):
+        if len(value) < 6:
+            raise ValidationError("Password must be at least 6 characters long.")
+
+class UserRegisterSchema(UserSchema):
+    email = fields.Email(required=True)
+    created_at = fields.DateTime(dump_only=True)
+    status = fields.Bool(dump_only=True)
+
+
+
--- routes/user.py
+++ routes/user.py
@@ -1,5 +1,10 @@
-from flask_smorest import Blueprint
+from flask_smorest import Blueprint, abort
 from flask.views import MethodView
+from models.user import User
+from passlib.hash import pbkdf2_sha256
+from models.schemas import UserRegisterSchema, UserSchema
+from sqlalchemy import or_
+from models.db import db
 
 blp = Blueprint("Users", "user", description="Operations on the user")
 
@@ -7,4 +12,23 @@ blp = Blueprint("Users", "user", description="Operations on the user")
 def UserRegister(MethodView):
 #Using MethodView allows the get or post method to handle GET or POST requests directly,
 #making it an alternative to using @blp.get or @blp.post.
+    @blp.arguments(UserRegisterSchema)
     def post(self, user_data):
+        if User.query.filter(
+            or_(
+                User.username == user_data["username"],
+                User.email == user_data["email"]
+            )
+        ).first():
+            abort(409, "A user with this username or email already exists.")
+
+        user = User(
+            username=user_data["username"],
+            email=user_data["email"],
+            password=pbkdf2_sha256.hash(user_data["password"])
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        return {"message": "User successfully created."}, 200
